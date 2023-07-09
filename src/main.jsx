5146482cde3468362316b6f7cf57ae952fc3d0f8
LouisBioum <hbioum@yahoo.fr>
2023-07-09 13:35:54 +0100

    Added host nested routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,15 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
 import Layout from "./components/Layout"
+import HostLayout from "./components/HostLayout"
+import "./index.css"
 
-import "./server"
+
+import "../server"
 
 function App() {
   return (
@@ -18,6 +24,12 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
