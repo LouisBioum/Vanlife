5717432868a13b776d67e0104623305f20ce4cbe
LouisBioum <hbioum@yahoo.fr>
2023-07-11 19:08:01 +0100

    Sad path (vandetail) and login page

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,10 +1,15 @@
 import React from "react"
-import { useParams, NavLink, Link, Outlet } from "react-router-dom"
-
+import { useParams, Link, NavLink, Outlet } from "react-router-dom"
 
 export default function HostVanDetail() {
     const { id } = useParams()
     const [currentVan, setCurrentVan] = React.useState(null)
+
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
 
     React.useEffect(() => {
         fetch(`/api/host/vans/${id}`)
@@ -14,12 +19,6 @@ export default function HostVanDetail() {
 
     if (!currentVan) {
         return <h1>Loading...</h1>
-    }
-    
-    const activeStyles = {
-        fontWeight: "bold",
-        textDecoration: "underline",
-        color: "#161616"
     }
     return (
         <section>
@@ -44,28 +43,27 @@ export default function HostVanDetail() {
                 </div>
 
                 <nav className="host-van-detail-nav">
-                <NavLink 
-                    to="."
-                    end
-                    style={({isActive}) => isActive ? activeStyles : null}
-                >
-                    Details
-                </NavLink>
-                <NavLink 
-                    to="pricing"
-                    style={({isActive}) => isActive ? activeStyles : null}
-                >
-                    Pricing
-                </NavLink>
-                <NavLink 
-                    to="photos"
-                    style={({isActive}) => isActive ? activeStyles : null}
-                >
-                    Photos
-                </NavLink>
+                    <NavLink
+                        to="."
+                        end
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        Details
+                    </NavLink>
+                    <NavLink
+                        to="pricing"
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        Pricing
+                    </NavLink>
+                    <NavLink
+                        to="photos"
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        Photos
+                    </NavLink>
                 </nav>
-
-                <Outlet context={ {currentVan}} />
+                <Outlet context={{ currentVan }} />
             </div>
         </section>
     )
