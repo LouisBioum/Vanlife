2f9ff8532842771fc0fac77c77019ac8866ba498
LouisBioum <hbioum@yahoo.fr>
2023-07-12 17:25:45 +0100

    Final touch (host van from fireabase, no auth)

--- src/api.js
+++ src/api.js
@@ -1,50 +1,53 @@
+import { initializeApp } from "firebase/app"
+import {
+    getFirestore,
+    collection,
+    doc,
+    getDocs,
+    getDoc,
+    query,
+    where
+} from "firebase/firestore/lite"
 
-import { initializeApp } from "firebase/app";
-import {getFirestore, collection, doc, getDocs, getDoc} from "firebase/firestore/lite"
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAW2b0K0hYl197JR41P1QNmWNX7njf-xNE",
-  authDomain: "vanlife-94689.firebaseapp.com",
-  projectId: "vanlife-94689",
-  storageBucket: "vanlife-94689.appspot.com",
-  messagingSenderId: "1080445695231",
-  appId: "1:1080445695231:web:4f314fe8cd9287f04f88ca"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
+// Refactoring the fetching functions below
 const vansCollectionRef = collection(db, "vans")
 
-
-// A function whose only purpose is to delay execution
-// for the specified # of milliseconds when used w/ `await`
-// e.g. inside an async function:
-// await sleep(2000)  => pauses the function for 2 seconds before moving on
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(() => resolve(), ms))
-}
-
-export async function getVans(id) {
+export async function getVans() {
     const snapshot = await getDocs(vansCollectionRef)
     const vans = snapshot.docs.map(doc => ({
-        ...doc.data(), // actual data of the doc
-        id: doc.id // not included by default by firestore
+        ...doc.data(),
+        id: doc.id
     }))
-    return(vans)
+    return vans
 }
 
 export async function getVan(id) {
     const docRef = doc(db, "vans", id)
     const snapshot = await getDoc(docRef)
-    return {...snapshot.data(), id: snapshot.id}
+    return {
+        ...snapshot.data(),
+        id: snapshot.id
+    }
 }
 
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
 
-// export async function getVans(id) {
-//     const url = id ? `/api/vans/${id}` : "/api/vans"
+// export async function getHostVans(id) {
+//     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
 //     const res = await fetch(url)
 //     if (!res.ok) {
 //         throw {
@@ -56,20 +59,6 @@ export async function getVan(id) {
 //     const data = await res.json()
 //     return data.vans
 // }
-
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
-}
 
 export async function loginUser(creds) {
     const res = await fetch("/api/login",
